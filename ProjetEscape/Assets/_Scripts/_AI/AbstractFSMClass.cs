ca4fa005f090aaf316f3cc284de737776d7f456c
RENCUREL <pierre.rencurel@orange.fr>
2019-10-11 15:44:55 -0400

    Class Setup
    DONE : Adding base FSM Setup classes
    TODO : Complete bringing FSM Framework to current project

---
+++ ProjetEscape/Assets/_Scripts/_AI/AbstractFSMClass.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public enum ExecutionState
+{
+    NONE,
+    ACTIVE,
+    COMPLETED,
+    TERMINATED,
+};
+
+public enum FSMStateType
+{
+    NONE,
+    PATROL,
+    ATTACK,
+};
+
+public abstract class AbstractFSMClass : ScriptableObject
+{
+
+    protected FiniteStateMachine _fsm;
+
+    public ExecutionState ExecutionState { get; protected set; }
+    public bool EnteredState { get; protected set; }
+    public FSMStateType FSMStateType { get; protected set; }
+
+    /// <summary>
+    /// Default setup : None active
+    /// </summary>
+    public virtual void OnEnabled()
+    {
+        ExecutionState = ExecutionState.NONE;
+    }
+
+    /// <summary>
+    /// Check if state is entered without issue
+    /// </summary>
+    /// <returns></returns>
+    public virtual bool EnterState()
+    {
+        //WIP
+
+        return true;
+    }
+
+
+    /// <summary>
+    /// Tells FSM to update current state
+    /// </summary>
+    public abstract void UpdateState();
+
+    /// <summary>
+    /// Check if state is exited without issue
+    /// </summary>
+    /// <returns></returns>
+    public virtual bool ExitState()
+    {
+        ExecutionState = ExecutionState.COMPLETED;
+        return true;
+    }
+}
---
+++ ProjetEscape/Assets/_Scripts/_AI/FiniteStateMachine.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FiniteStateMachine : MonoBehaviour
+{
+
+    [SerializeField]
+    AbstractFSMClass _startingState;
+    AbstractFSMClass _currentState;
+
+    [SerializeField]
+    List<AbstractFSMClass> _validStates;
+
+    Dictionary<FSMStateType, AbstractFSMClass> _fsmStates;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
