6d5b12794dfb875b046d7c94a9a022c75fb34d60
RENCUREL <pierre.rencurel@orange.fr>
2019-10-16 16:26:35 -0400

    DetectionSetup
    DONE : Setup a sight based detection model
    TODO : Implement messages and state transitions

---
+++ ProjetEscape/Assets/_Scripts/_AI/Senses/Sight.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets._Scripts._AI.Senses
+{
+    public class Sight : Sense
+    {
+        protected int fov = 45;
+        protected int viewRange = 30;
+
+        private Transform playerTrans;
+        private Vector3 rayDirection;
+
+        protected override void Initialize()
+        {
+            playerTrans = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+
+        protected override void UpdateSenses()
+        {
+            elapsedTime = Time.deltaTime;
+            if (elapsedTime >= detectRate)
+                DetectPlayer();
+        }
+
+        private void DetectPlayer()
+        {
+            RaycastHit hit;
+
+            rayDirection = playerTrans.position - transform.position;
+
+            if((Vector3.Angle(rayDirection, transform.forward)) < fov)
+            {
+                if(Physics.Raycast(transform.position, rayDirection, out hit, viewRange))
+                {
+                    Player player = hit.collider.GetComponent<Player>();
+
+                    if (player != null)
+                        Debug.Log("Player detected");
+                }
+            }
+        }
+
+        void OnDrawGizmos()
+        {
+            if (!Application.isEditor || playerTrans == null) return;
+            Debug.DrawLine(transform.position, playerTrans.position, Color.red);
+            Vector3 frontRayPoint = transform.position +
+            (transform.forward * viewRange);
+            //Approximate perspective visualization
+            Vector3 leftRayPoint = Quaternion.Euler(0, fov * 0.5f, 0) *
+            frontRayPoint;
+            Vector3 rightRayPoint = Quaternion.Euler(0, -fov * 0.5f, 0) *
+            frontRayPoint;
+            Debug.DrawLine(transform.position, frontRayPoint, Color.green);
+            Debug.DrawLine(transform.position, leftRayPoint, Color.green);
+            Debug.DrawLine(transform.position, rightRayPoint, Color.green);
+        }
+    }
+}
---
+++ ProjetEscape/Assets/_Scripts/_AI/States/AttackState.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets._Scripts._AI.States
+{
+    [CreateAssetMenu(fileName = "AttackState", menuName = "ProjetEscape/States/Attack", order = 3)]
+    public class AttackState : AbstractFSMClass
+    {
+        public override void OnEnable()
+        {
+            base.OnEnable();
+            FSMStateType = FSMStateType.ATTACK;
+        }
+
+        public override bool EnterState()
+        {
+            EnteredState = false;
+            if (base.EnterState())
+            {
+                //TO IMPLEMENT
+            }
+
+            return EnteredState;
+        }
+
+        //TO DO
+        public override void UpdateState()
+        {
+            if (EnteredState)
+            {
+
+            }
+        }
+
+        public override bool ExitState()
+        {
+            base.ExitState();
+
+            return true;
+        }
+    }
+}
